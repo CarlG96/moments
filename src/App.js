998662f874d741ed0a7f5976f86244b39a07ea1b
CarlG96 <carl.gardiner1@hotmail.com>
2022-11-08 12:06:46 +0000

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { Navbar } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          // How to deal with a route not found error
+          <Route render={()=> <p>Page not found!</p>}/>
+        </Switch>
+      </Container>
     </div>
   );
 }
